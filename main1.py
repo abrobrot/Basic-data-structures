9e45997d5fe4dbb6d32668fadb600dca053c3d6f
abrobrot <olegstakhov210@gmail.com>
2024-10-18 00:48:20 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,35 @@
+# 1st program
+print ((9**0.5)*5)
+
+# 2st program
+print (9.99 > 9.98 and 1000 != 1000.1 )
+
+# 3st program
+print (2*2+2)
+print (2*(2+2))
+print ((2*2+2) == (2*(2+2)))
+
+# 4st program
+print (int (float ('123.456') *10) %10)
+
+# 3 и 4 ver2 irrational
+print ("__________________________ повторение 3 и 4 задачи в последовательном виде")
+#3st program
+a =(2*2+2)
+print (a)
+b = (2*(2+2))
+print (b)
+print (a == b)
+# 4st program
+a = '123.456'
+b = float (a)
+c = b *10
+d = int (c)
+f = d %10
+print (f)
+
+
+
+
+
+
